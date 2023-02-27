8e3a5b76afce615b9fec8aa60871719cb4f04a0c
Anna Sokur <115190892+AnnaSokur@users.noreply.github.com>
2023-02-27 23:23:45 +0100

    Add HW 8

---
+++ HomeWork8/src/main/java/Tasks.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Tasks {
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int complement = target - numbers[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(numbers[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+
